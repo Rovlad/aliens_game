33f6afae898277eef39b07bea52467326c1d0ddf
Rovlad <abnvrz@gmail.com>
2023-01-30 19:25:51 +0000

    All done apart from scoring

--- ship.py
+++ ship.py
@@ -22,5 +22,9 @@ class Ship:
             self.x -= self.settings.ship_speed
         self.rect.x = self.x
 
+    def center_ship(self):
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
